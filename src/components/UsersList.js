6d9929f980be09d26e3efa1d757a5c56d049ebd2
chilliflakes5790 <thakaliaayush@gmail.com>
2025-07-26 17:16:12 +0545

    feat: useThunk hook added

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,38 +1,58 @@
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import {  useSelector } from "react-redux";
 import { fetchUsers, addUser } from "../store";
 import Skeleton from "./Skeleton";
 import Button from "./Button";
+import { useThunk } from "../hooks/use-thunk";
 
 function UsersList() {
-  const dispatch = useDispatch();
+ const [doFetchUsers, isLoadingUsers, loadingUsersError] = useThunk(fetchUsers);
+ const [doCreateUser, isCreatingUser, creatingUserError] = useThunk(addUser);
 
-  const { isLoading, data, error } = useSelector((state) => {
+  const { data } = useSelector((state) => {
     return state.users;
   });
 
   useEffect(() => {
-    dispatch(fetchUsers());
-  }, [dispatch]);
+   doFetchUsers();
+  }, [doFetchUsers]);
 
-  if (isLoading) {
+  const handleUserAdd = () => {
+    doCreateUser();
+  };
+
+  if (isLoadingUsers) {
     return <Skeleton times={6} className={"h-10 w-full"} />;
   }
 
-  if (error) {
+  if (loadingUsersError) {
     return <div>Error fetching data...</div>;
   }
 
   const renderedUsers = data.map((user) => {
-    return <div key={user.id} className="mb-2 border rounded">
-      <div className="flex p-2 justify-between items-center cursor-pointer">
-        {user.name}
+    return (
+      <div key={user.id} className="mb-2 border rounded">
+        <div className="flex p-2 justify-between items-center cursor-pointer">
+          {user.name}
+        </div>
       </div>
-    </div>;
+    );
   });
 
-
-  return <div>{renderedUsers}</div>;
+  return (
+    <div>
+      <div className="flex flex-row justify-between m-3">
+        <h1 className="m-2 text-xl">Users</h1>
+        {isCreatingUser ? (
+          "Creating User..."
+        ) : (
+          <Button onClick={handleUserAdd}>+ Add User</Button>
+        )}
+        {creatingUserError && "Error creating user..."}
+      </div>
+      {renderedUsers}
+    </div>
+  );
 }
 
 export default UsersList;
