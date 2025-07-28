59d3a9c0cb85686f4c4ab2027f85644a71a763f8
chilliflakes5790 <thakaliaayush@gmail.com>
2025-07-28 18:51:03 +0545

    feat:AlbumsListItem added

---
+++ src/components/AlbumsListItem.js
@@ -0,0 +1,28 @@
+import Button from "./Button";
+import ExpandablePanel from "./ExpandablePanel";
+import { GoTrashcan } from "react-icons/go";
+import { useRemoveAlbumMutation } from "../store";
+
+function AlbumsListItem({ album }) {
+  const [removeAlbum, results] = useRemoveAlbumMutation();
+
+  const handleRemoveAlbum = () => {
+    removeAlbum(album);
+  };
+
+  const header = (
+    <div>
+      <Button loading={results.onLoading} onClick={handleRemoveAlbum}>
+        <GoTrashcan />
+      </Button>
+      {album.title}
+    </div>
+  );
+  return (
+    <ExpandablePanel key={album.id} header={header}>
+      List of photos in album
+    </ExpandablePanel>
+  );
+}
+
+export default AlbumsListItem;
--- src/store/apis/albumsApi.js
+++ src/store/apis/albumsApi.js
@@ -20,6 +20,15 @@ const albumsApi = createApi({
   }),
   endpoints(builder) {
     return {
+      removeAlbum: builder.mutation({
+        query: (album) => {
+          return {
+            url: `/albums/${album.id}`,
+            method: "DELETE",
+          };
+        },
+      }),
+
       addAlbum: builder.mutation({
         invalidatesTags: (result, error, user) => {
           return [{ type: "Album", id: user.id }];
@@ -35,6 +44,7 @@ const albumsApi = createApi({
           };
         },
       }),
+
       fetchAlbums: builder.query({
         providesTags: (result, error, user) => {
           return [{ type: "Album", id: user.id }];
@@ -53,5 +63,5 @@ const albumsApi = createApi({
   },
 }); //automatically creates slice
 
-export const { useFetchAlbumsQuery, useAddAlbumMutation } = albumsApi;
+export const { useFetchAlbumsQuery, useAddAlbumMutation, useRemoveAlbumMutation } = albumsApi;
 export { albumsApi };
